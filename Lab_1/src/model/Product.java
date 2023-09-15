b4da174edb5d204d6e6465fd822a80863b3860ac
harshachowdhary <144712425+harshachowdhary@users.noreply.github.com>
2023-09-15 15:08:03 -0400

    Add files via upload

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author harsh
+ */
+public class Product {
+   
+    private String name;
+    private String descr;
+    private String avainNum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getAvainNum() {
+        return avainNum;
+    }
+
+    public void setAvainNum(String avainNum) {
+        this.avainNum = avainNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+    
+}
